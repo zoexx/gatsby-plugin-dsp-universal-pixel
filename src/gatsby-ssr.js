fc383bc16e591a74d21869fb0d025c148281a018
zoeying <296852455@qq.com>
2020-01-18 03:19:34 +0800

    fix cntrData string

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   if (process.env.NODE_ENV === `production`) {
+    let cntrDataInject = "";
+    if (pluginOptions.cntrData !== undefined) {
+      cntrDataInject = `var cntrData = JSON.parse('${JSON.stringify(pluginOptions.cntrData)}');`
+    }
     return setHeadComponents([
       <script src="https://cdn01.basis.net/assets/up.js?um=0"></script>,
       <script
         key={`gatsby-plugin-dsp-universal-pixel`}
         dangerouslySetInnerHTML={{
           __html: `
-          if(${pluginOptions.cntrData !== undefined}){ var cntrData = JSON.parse('${JSON.stringify(pluginOptions.cntrData)}')}
+          ${cntrDataInject && cntrDataInject}
           cntrUpTag.track('cntrData', '${pluginOptions.id}');
       `,
         }}
